91dc7ca7a22efebad34f6efbd17517320ea70510
Treast <vincent.riva@outlook.fr>
2019-09-16 06:04:05 +0200

    Fix audit

--- src/app/js/Canvas.ts
+++ src/app/js/Canvas.ts
@@ -1,13 +1,8 @@
 import { Vector2 } from './utils/Vector2';
-import Perspective from './utils/Perspective';
 
 class Canvas {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
-  private corners: Vector2[] = [];
-  private position: Vector2 = new Vector2(0.5, 0.5);
-  private nextPosition: Vector2 = new Vector2(0.5, 0.5);
-  private rawPoint: Vector2 = new Vector2(0.5, 0.5);
   constructor() {
     this.canvas = document.querySelector('canvas');
     this.canvas.width = window.innerWidth;
@@ -19,23 +14,6 @@ class Canvas {
     requestAnimationFrame(() => this.render());
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fillStyle = 'red';
-
-    const positionX = this.lerp(this.position.x, this.nextPosition.x, 0.1);
-    const positionY = this.lerp(this.position.y, this.nextPosition.y, 0.1);
-    this.position = new Vector2(positionX, positionY);
-
-    if (Perspective.hasMatrix()) {
-      Perspective.computePoint(this.rawPoint).then((point: number[]) => {
-        this.ctx.fillRect(point[0] * window.innerWidth - 10, point[1] * window.innerHeight - 10, 20, 20);
-      });
-    } else {
-      this.ctx.fillRect(this.position.x - 10, this.position.y - 10, 20, 20);
-    }
-  }
-
-  setPosition(x: number, y: number) {
-    this.nextPosition = new Vector2(x * window.innerWidth, y * window.innerHeight);
-    this.rawPoint = new Vector2(x, y);
   }
 
   lerp(a: number, b: number, n: number) {
--- src/app/js/main.ts
+++ src/app/js/main.ts
@@ -1,29 +1,8 @@
 import App from './utils/App';
-// @ts-ignore
-import io from 'socket.io-client';
 import Canvas from './Canvas';
-import Perspective from './utils/Perspective';
-import { Vector2 } from './utils/Vector2';
-
-interface DronePosition {
-  x: number;
-  y: number;
-}
 
 const app = new App();
 
 app.isReady().then(() => {
-  const socket = io('https://dronie.vincentriva.fr');
-  socket.on('connect', () => console.log('Connected to socket'));
-  socket.on('disconnect', () => console.log('Disconnected to socket'));
-
-  socket.on('DRONE:CALIBRATE', (data: DronePosition) => {
-    console.log('Corner', data.x, data.y);
-    Perspective.addCorners(new Vector2(data.x, data.y));
-  });
-
-  socket.on('DRONE:DETECT', (data: any) => {
-    Canvas.setPosition(data.x, data.y);
-  });
   Canvas.render();
 });
